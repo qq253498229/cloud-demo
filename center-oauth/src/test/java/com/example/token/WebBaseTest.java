e13d40ba3d51788c472a0a5aa00df84cdec5dc35
大胃王 <253498229@qq.com>
2018-05-09 16:20:38 +0800

    review

--- center-oauth/src/test/java/com/example/token/WebBaseTest.java
+++ center-oauth/src/test/java/com/example/token/WebBaseTest.java
@@ -6,15 +6,23 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -49,5 +57,44 @@ public class WebBaseTest {
 
   protected String decode(String str) {
     return new String(Base64.getDecoder().decode(str.getBytes()));
+  }
+
+  protected Map getTokenMap() throws IOException, URISyntaxException {
+    Map map = new HashMap();
+    map.put("client_id", clientId);
+    map.put("redirect_uri", "http://www.baidu.com");
+    map.put("port", port);
+
+    ResponseEntity<String> response;
+
+    response = restTemplate.withBasicAuth(username, password)
+            .postForEntity("http://localhost:{port}/oauth/authorize" +
+                            "?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}"
+                    , null, String.class, map);
+    assertEquals(response.getStatusCode(), OK);
+    assertNotNull(response.getBody());
+
+    List<String> setCookie = response.getHeaders().get("Set-Cookie");
+    String jSessionIdCookie = setCookie.get(0);
+    String cookieValue = jSessionIdCookie.split(";")[0];
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Cookie", cookieValue);
+    response = restTemplate.withBasicAuth(username, password)
+            .postForEntity("http://localhost:{port}/oauth/authorize" +
+                            "?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&user_oauth_approval=true&authorize=Authorize"
+                    , new HttpEntity<>(headers), String.class, map);
+    assertEquals(response.getStatusCode(), FOUND);
+    assertNull(response.getBody());
+
+    String location = response.getHeaders().get("Location").get(0);
+    String query = new URI(location).getQuery();
+    response = new TestRestTemplate(clientId, secret)
+            .postForEntity("http://localhost:{port}/oauth/token" +
+                            "?" + query + "&grant_type=authorization_code&redirect_uri={redirect_uri}"
+                    , null, String.class, map);
+    assertEquals(response.getStatusCode(), OK);
+    assertNotNull(response.getBody());
+
+    return mapper.readValue(response.getBody(), HashMap.class);
   }
 }
