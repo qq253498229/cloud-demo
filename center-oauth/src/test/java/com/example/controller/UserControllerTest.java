f678b916748a7ffe1d57b566d8f9adc53995e52a
wangbin <253498229@qq.com>
2018-05-09 21:38:54 +0800

    加入用户endpoint测试

--- center-oauth/src/test/java/com/example/controller/UserControllerTest.java
+++ center-oauth/src/test/java/com/example/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,19 +10,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
+@Slf4j
 public class UserControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
   @Test
-  @WithMockUser(roles = "USER", username = "user")
+  @WithMockUser(roles = "USER")
   public void getUser() throws Exception {
     mockMvc.perform(get("/user"))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("username").value("user"))
+            .andExpect(jsonPath("password").value("password"))
+            .andExpect(jsonPath("authorities").isNotEmpty())
+    ;
   }
 }
 
