17bef586d845a0f156d587f990c2b5b0143a2268
大胃王 <253498229@qq.com>
2018-05-10 17:16:40 +0800

    review

--- center-oauth/src/test/java/com/example/CenterOauthApplicationTests.java
+++ center-oauth/src/test/java/com/example/CenterOauthApplicationTests.java
@@ -5,10 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest
 public class CenterOauthApplicationTests {
   @Test
   public void contextLoads() {
