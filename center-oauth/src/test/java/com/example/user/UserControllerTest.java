251bd107ba864c6e59c52b0392c0c505012d7711
大胃王 <253498229@qq.com>
2018-05-23 10:53:24 +0800

    polish

--- center-oauth/src/test/java/com/example/user/UserControllerTest.java
+++ center-oauth/src/test/java/com/example/user/UserControllerTest.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
+@TestPropertySource(properties = {"logging.level.org.springframework.security=debug"})
 public class UserControllerTest {
   @Autowired
   private MockMvc mockMvc;
@@ -28,6 +31,14 @@ public class UserControllerTest {
             .andExpect(jsonPath("username").value("user"))
             .andExpect(jsonPath("password").value("password"))
             .andExpect(jsonPath("enabled").value(true))
+            .andDo(print())
+    ;
+  }
+
+  @Test
+  public void testLogin() throws Exception {
+    mockMvc.perform(formLogin())
+            .andExpect(status().isFound())
             .andDo(print())
     ;
   }
